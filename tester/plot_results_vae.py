49e694d65f1771f6ec919af2b68e8b70f6b55843
tunglm <tunglm2203@kaist.ac.kr>
2020-01-03 14:20:08 +0900

    Plot another information when training VAE

--- tester/plot_results_vae.py
+++ tester/plot_results_vae.py
@@ -25,9 +25,17 @@ def load_results(file):
 def main():
     path = '/mnt/hdd/tung/workspace/rlkit/data/01-02-rlkit-pusher-rig-example/01-02-rlkit-pusher-rig-example_2020_01_02_13_14_05_0000--s-21885/vae_progress.csv'
     res = load_results(path)
+
     recon_mse = 'debug/MSE of reconstruction'
     random_mses = 'debug/MSE of random decoding'
     mse_improvement = 'debug/MSE improvement over random'
+
+    train_loss = 'train/loss'
+    train_logprob = 'train/Log Prob'
+    train_kle = 'train/KL'
+    test_loss = 'test/loss'
+    test_logprob = 'test/Log Prob'
+    test_kle = 'test/KL'
 
     mean_postfix = ' Mean'
     min_postfix = ' Min'
@@ -65,6 +73,28 @@ def main():
     plt.plot(res[mse_improvement + std_postfix])
     plt.title('Debug (Std)')
     plt.legend([recon_mse, random_mses + std_postfix, mse_improvement + std_postfix])
+
+    # Loss variance
+    plt.figure()
+    plt.plot(res[train_loss])
+    plt.plot(res[test_loss])
+    plt.title('Debug (Loss)')
+    plt.legend([train_loss, test_loss])
+
+    # Log prob
+    plt.figure()
+    plt.plot(res[train_logprob])
+    plt.plot(res[test_logprob])
+    plt.title('Debug (Log Probability)')
+    plt.legend([train_logprob, test_logprob])
+
+    # KL
+    plt.figure()
+    plt.plot(res[train_kle])
+    plt.plot(res[test_kle])
+    plt.title('Debug (KL)')
+    plt.legend([train_kle, test_kle])
+
     plt.show()
 
 
