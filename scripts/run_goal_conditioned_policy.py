fa663bd90f7a9605ecaa3e8aef713de5cd4e9043
tunglm <tunglm2203@kaist.ac.kr>
2020-01-09 14:28:44 +0900

    Modify to load learned model using torch.load

--- scripts/run_goal_conditioned_policy.py
+++ scripts/run_goal_conditioned_policy.py
@@ -5,16 +5,17 @@ from rlkit.core import logger
 from rlkit.samplers.rollout_functions import multitask_rollout
 from rlkit.torch import pytorch_util as ptu
 from rlkit.envs.vae_wrapper import VAEWrappedEnv
+import torch
 
 
 def simulate_policy(args):
-    data = pickle.load(open(args.file, "rb"))
+    data = torch.load(open(args.file, "rb"))
     policy = data['evaluation/policy']
     env = data['evaluation/env']
     print("Policy and environment loaded")
     if args.gpu:
         ptu.set_gpu_mode(True)
-        policy.to(ptu.device)
+        # policy.to(ptu.device)
     if isinstance(env, VAEWrappedEnv) and hasattr(env, 'mode'):
         env.mode(args.mode)
     if args.enable_render or hasattr(env, 'enable_render'):
