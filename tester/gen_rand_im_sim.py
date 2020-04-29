586a6236fe107af4e69fc26b1f6c2bcb14012a81
tunglm <tunglm2203@kaist.ac.kr>
2020-04-29 17:14:14 +0900

    Modify generating data function to store coordinate of EE and OB

--- tester/gen_rand_im_sim.py
+++ tester/gen_rand_im_sim.py
@@ -18,15 +18,15 @@ def main():
     # ======================== USER SCOPE  ========================
     root_path = '/home/tung/workspace/rlkit/tester/'
 
-    env_id = 'SawyerPushNIPSEasy-v0'
-    # env_id = 'SawyerPushNIPSCustomEasy-v0'
+    # env_id = 'SawyerPushNIPSEasy-v0'
+    env_id = 'SawyerPushNIPSCustomEasy-v0'
     # env_id = 'SawyerPushNIPS-v0'
 
-    n_samples = 500
+    n_samples = 10000
     imsize = 48
     n_samples_to_reset = 1
-    data_folder = 'rand_img_sim.{}'.format(n_samples)
-    # data_folder = 'rand_img_sim_tgt.{}'.format(n_samples)
+    # data_folder = 'rand_img_sim.{}'.format(n_samples)
+    data_folder = 'rand_img_sim_tgt_new.{}'.format(n_samples)
     key_img = 'image_desired_goal'
     # key_img = 'image_observation'
 
@@ -35,25 +35,34 @@ def main():
     if not os.path.exists(save_path):
         os.mkdir(save_path)
     # Create environment
-    env_sim = gym.make(env_id)
+    env_sim = gym.make(env_id,
+                       hand_goal_low=(-0.2, 0.5),
+                       hand_goal_high=(0.2, 0.7),
+                       puck_goal_low=(-0.2, 0.5),
+                       puck_goal_high=(0.2, 0.7),
+                       )
     env_sim = ImageEnv(env_sim,
                        imsize=imsize,
                        normalize=True,
                        transpose=True,
-                       init_camera=sawyer_init_camera_zoomed_in_aim_v0,
-                       # init_camera=sawyer_init_camera_zoomed_in_aim_v1,
+                       # init_camera=sawyer_init_camera_zoomed_in_aim_v0,
+                       init_camera=sawyer_init_camera_zoomed_in_aim_v1,
                        )
     env_sim.reset()
 
+    episodes = []
     # Generating data
     for i in tqdm(range(n_samples)):
+        episode = []
         if i % n_samples_to_reset == 0:
             o = env_sim.reset()
         else:
             o, _, _, _ = env_sim.step(env_sim.action_space.sample())
         img = o[key_img]
         g = img
-
+        obs = dict(ee_pos=o['state_desired_goal'][:2],
+                   obj_pos=o['state_desired_goal'][2:])
+        episode.append(obs)
         im_show = cv2.resize(g.reshape(3, imsize, imsize).transpose()[:, :, ::-1], (128, 128))
         cv2.imshow('observation', im_show)
         cv2.waitKey(1)
@@ -61,6 +70,11 @@ def main():
         filename = os.path.join(save_path, '{}'.format(i))
         cv2.imwrite(filename + '.png', unormalize_image(im_show))
         np.savez_compressed(filename, im=img)
+
+        episodes.append(np.array(episode))
+    # Save episode's data
+    save_traj_path = os.path.join(save_path, 'random_trajectories.npz')
+    np.savez_compressed(save_traj_path, data=episodes)
 
 
 if __name__ == '__main__':
